8320ba1a1ccdfa5ed09d83b56fd99eea19353269
Edward Yin <59598698+kaixing2012@users.noreply.github.com>
2021-08-14 22:27:31 +0800

    install redux needed package

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiService } from './shared/services/api.service';
@@ -13,7 +16,10 @@ import { ApiService } from './shared/services/api.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument(),
+    EffectsModule.forRoot([])
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
