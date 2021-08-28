eaf263372cb72eb9f6c34529f817ca5751762b42
Edward Yin <59598698+kaixing2012@users.noreply.github.com>
2021-08-28 20:09:28 +0800

    apply new data service method to request data from store first then backend service

--- project/src/app/featured/pages/user/user.component.ts
+++ project/src/app/featured/pages/user/user.component.ts
@@ -1,42 +1,54 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { User } from 'src/app/shared/models/user.model';
-import { ResponseBody } from './../../../shared/models/api-response.model';
+import { unsortedOrder } from 'src/app/shared/overloads/keyvalue.overload';
+import { ApiData } from './../../../shared/models/api-data.model';
 import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserComponent implements OnInit, AfterViewInit {
-
-  users$: Observable<ResponseBody<User>>;
+  users$: Observable<ApiData<User>>;
   userSubject = new Subject<void>();
 
   pageNum = 'page=1';
 
-  constructor(private readonly userService: UserService) { }
+  get unsortedOrder(): any {
+    return unsortedOrder;
+  }
+
+  constructor(private readonly userService: UserService) {}
 
   ngOnInit() {
     this.users$ = this.userSubject.pipe(
       switchMap(() => this.userService.getUsers(this.pageNum)
         .pipe(
-          tap((res) => this.pageNum = res.meta.pagination.links.next.split('?')[1])
+          switchMap((res) => {
+            if (res.status === 'SUCCESS'){
+              this.pageNum = res.collection.meta.pagination.links.next.split('?')[1];
+            }
+
+            return of(res);
+          })
         )
       ),
     );
   }
 
   ngAfterViewInit(): void {
-    this.userSubject.next();
+    setTimeout(() => {
+      this.userSubject.next();
+    }, 10);
   }
 
   onPrevious(currentPage: number): void {
-    if ((currentPage - 1) >= 1){
-      this.pageNum = `page=${currentPage -= 1}`;
+    if (currentPage - 1 >= 1) {
+      this.pageNum = `page=${(currentPage -= 1)}`;
       this.userSubject.next();
     }
   }
