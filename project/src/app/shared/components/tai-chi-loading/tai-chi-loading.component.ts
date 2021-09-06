8acfb9adfcd87ae8c15c427123bcf21c6053a69f
Edward Yin <59598698+kaixing2012@users.noreply.github.com>
2021-09-06 13:42:03 +0800

    add tai chi loading shared components

---
+++ project/src/app/shared/components/tai-chi-loading/tai-chi-loading.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tai-chi-loading',
+  templateUrl: './tai-chi-loading.component.html',
+  styleUrls: ['./tai-chi-loading.component.scss']
+})
+export class TaiChiLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void { }
+}
